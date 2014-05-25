7a20a767444265cd57d11a1cead7cebf77b7d41d
David Leoni <davidleonigit-github@yahoo.com>
2014-05-25 14:37:38 +0200

    added calls to getAttrDefName and Description in checkEntityType() method

--- src/main/java/eu/trentorise/opendata/semantics/IntegrityChecker.java
+++ src/main/java/eu/trentorise/opendata/semantics/IntegrityChecker.java
@@ -193,6 +193,20 @@ public class IntegrityChecker {
                 throw new IntegrityException("Found invalid attr def in etype " + etype.getURL(), ex);
             }
         }
+        
+        
+        try {
+            etype.getNameAttrDef();
+        } catch (Exception ex){
+            throw new IntegrityException("Found problem in getNameAttrDef()", ex);
+        }
+        
+        try {
+            etype.getDescriptionAttrDef();
+        } catch (Exception ex){
+            throw new IntegrityException("Found problem in getNameAttrDef()", ex);
+        }
+        
 
     }
 
