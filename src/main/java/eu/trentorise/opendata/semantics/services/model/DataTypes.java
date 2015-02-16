520503d090cbe780ac4201a2b5af5ba83de0a59f
David Leoni <davidleonigit-github@yahoo.com>
2015-02-16 16:52:07 +0100

    added DataTypes.datatypeUrlToMeaningKind

--- src/main/java/eu/trentorise/opendata/semantics/services/model/DataTypes.java
+++ src/main/java/eu/trentorise/opendata/semantics/services/model/DataTypes.java
@@ -20,6 +20,7 @@ package eu.trentorise.opendata.semantics.services.model;
 import eu.trentorise.opendata.semantics.model.entity.IEntity;
 import eu.trentorise.opendata.semantics.model.entity.IStructure;
 import eu.trentorise.opendata.semantics.model.knowledge.IConcept;
+import eu.trentorise.opendata.semtext.MeaningKind;
 import eu.trentorise.opendata.semtext.SemText;
 import java.util.Collections;
 import java.util.Date;
@@ -33,7 +34,8 @@ import java.util.Set;
  * <br/>
  * Primitive datatypes are a subset of <a
  * target="_blank" href="http://www.w3.org/TR/2004/REC-rdf-mt-20040210/">
- * RDF Semantics, XSD datatypes section</a>. Complex types such as <i>oe:structure</i>
+ * RDF Semantics, XSD datatypes section</a>. Complex types such as
+ * <i>oe:structure</i>
  * and <i>oe:entity</i> are newly defined. <br/>
  * <br/>
  * Datatypes are mapped to Java objects according to the following scheme: <br/>
@@ -44,14 +46,20 @@ import java.util.Set;
  * INTEGER : {@link java.lang.Integer} <br/>
  * FLOAT : {@link java.lang.Float} <br/>
  * LONG : {@link java.lang.Long} <br/>
- * CONCEPT : {@link eu.trentorise.opendata.semantics.model.knowledge.IConcept} <br/>
- * SEMANTIC_TEXT : {@link eu.trentorise.opendata.semtext.SemText} <br/> 
- * ENTITY : {@link eu.trentorise.opendata.semantics.model.entity.IEntity} A convenient implementation for entities which are values of other entities (like in part-of relations) may be found in the {@link eu.trentorise.opendata.semantics.impl.model.entity.MinimalEntity} class <br/> 
- * STRUCTURE : {@link eu.trentorise.opendata.semantics.model.entity.IStructure} <br/>
+ * CONCEPT : {@link eu.trentorise.opendata.semantics.model.knowledge.IConcept}
+ * <br/>
+ * SEMANTIC_TEXT : {@link eu.trentorise.opendata.semtext.SemText} <br/>
+ * ENTITY : {@link eu.trentorise.opendata.semantics.model.entity.IEntity} A
+ * convenient implementation for entities which are values of other entities
+ * (like in part-of relations) may be found in the
+ * {@link eu.trentorise.opendata.semantics.impl.model.entity.MinimalEntity}
+ * class <br/>
+ * STRUCTURE : {@link eu.trentorise.opendata.semantics.model.entity.IStructure}
+ * <br/>
  *
  * @author Ivan Tankoyeu <tankoyeu@disi.unitn.it>
  * @author David Leoni <david.leoni@unitn.it>
- * 
+ *
  *
  */
 public final class DataTypes {
@@ -69,7 +77,7 @@ public final class DataTypes {
     public static final String SEMANTIC_TEXT = "oe:semantic-text";
     public static final String STRUCTURE = "oe:structure";
     public static final String ENTITY = "oe:entity";
-    
+
     public static final String ATTRDEF = "oe:attrdef";
     public static final String ETYPE = "oe:etype";
 
@@ -87,7 +95,7 @@ public final class DataTypes {
         DATATYPE_PRETTY_NAMES_EN.put(FLOAT, "Single precision number");
         DATATYPE_PRETTY_NAMES_EN.put(LONG, "Long integer");
         DATATYPE_PRETTY_NAMES_EN.put(CONCEPT, "Concept");
-        DATATYPE_PRETTY_NAMES_EN.put(SEMANTIC_TEXT, "Semantic text");        
+        DATATYPE_PRETTY_NAMES_EN.put(SEMANTIC_TEXT, "Semantic text");
         DATATYPE_PRETTY_NAMES_EN.put(STRUCTURE, "Structure");
         DATATYPE_PRETTY_NAMES_EN.put(ENTITY, "Entity");
         DATATYPE_PRETTY_NAMES_MAP.put(Locale.ENGLISH, DATATYPE_PRETTY_NAMES_EN);
@@ -99,7 +107,7 @@ public final class DataTypes {
         DATATYPE_PRETTY_NAMES_IT.put(FLOAT, "Numero a precisione singola");
         DATATYPE_PRETTY_NAMES_IT.put(LONG, "Intero grande");
         DATATYPE_PRETTY_NAMES_IT.put(CONCEPT, "Concetto");
-        DATATYPE_PRETTY_NAMES_IT.put(SEMANTIC_TEXT, "Testo semantico");        
+        DATATYPE_PRETTY_NAMES_IT.put(SEMANTIC_TEXT, "Testo semantico");
         DATATYPE_PRETTY_NAMES_IT.put(STRUCTURE, "Struttura");
         DATATYPE_PRETTY_NAMES_IT.put(ENTITY, "Entità");
         DATATYPE_PRETTY_NAMES_MAP.put(Locale.ITALIAN, DATATYPE_PRETTY_NAMES_IT);
@@ -111,7 +119,7 @@ public final class DataTypes {
         JAVA_DATATYPES.put(FLOAT, Float.class);
         JAVA_DATATYPES.put(LONG, Long.class);
         JAVA_DATATYPES.put(CONCEPT, IConcept.class);
-        JAVA_DATATYPES.put(SEMANTIC_TEXT, SemText.class);        
+        JAVA_DATATYPES.put(SEMANTIC_TEXT, SemText.class);
         JAVA_DATATYPES.put(ENTITY, IEntity.class);
         JAVA_DATATYPES.put(STRUCTURE, IStructure.class);
 
@@ -155,7 +163,23 @@ public final class DataTypes {
         return Collections.unmodifiableSet(DATATYPE_PRETTY_NAMES_MAP.keySet());
     }
 
-    private DataTypes(){
-        
+    private DataTypes() {
+
+    }
+
+    /**
+     * Returns the MeaningKind corresponding to the given datatype url.
+     *
+     * @throws IllegalArgumentException if url doesn't correspond to entity or
+     * concept datatypes.
+     */
+    public static MeaningKind datatypeUrlToMeaningKind(String datatypeUrl) {
+        if (datatypeUrl.contains(ENTITY)) {
+            return MeaningKind.ENTITY;
+        }
+        if (datatypeUrl.contains(CONCEPT)) {
+            return MeaningKind.CONCEPT;
+        }
+        throw new IllegalArgumentException("Tried to get a MeaningKind from an unsupported datatype url! Found url: " + datatypeUrl);
     }
 }
